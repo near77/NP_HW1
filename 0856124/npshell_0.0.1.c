2dbaa83d5176bbd3271ca706e80ad0a7d8c08235
near77 <near6334@gmail.com>
2019-10-06 17:21:04 +0800

    pipe test

--- 0856124/npshell_0.0.1.c
+++ 0856124/npshell_0.0.1.c
@@ -8,8 +8,6 @@
 #define RL_BUFSIZE 1024
 #define TOK_BUFSIZE 64
 #define TOK_DELIMITERS " \t\r\n\a"
-#define CMD_DELIMITERS ">"
-#define ARRAY_SIZE(x) (sizeof((x))/sizeof(*(x)))
 
 //--------built in function-------------
 char *builtin_str[] = {
@@ -20,7 +18,7 @@ char *builtin_str[] = {
 
 int sh_exit(char **args)
 {
-    exit(0);
+    return 0;
 }
 
 int sh_printenv(char **args)
@@ -44,41 +42,6 @@ int num_builtins() {
     return sizeof(builtin_str) / sizeof(char *);
 }
 //-------------------------------------
-
-static inline void error(const char *msg)
-{
-    perror(msg);
-    exit(EXIT_FAILURE);
-}
-
-int cisshPipe(char **command1, char **command2)
-{
-    int fd[2];
-    pid_t childPid;
-    if (pipe(fd) != 0)
-        error("failed to create pipe");
-
-    if ((childPid = fork()) == -1)
-        error("failed to fork");
-
-    if (childPid == 0)
-    {
-        close(fd[0]);
-        dup2(fd[1], STDOUT_FILENO);
-        close(fd[1]);
-        execvp(command1[0], command1);
-        error("failed to exec command 1");
-    }
-    else
-    {
-        close(fd[1]);
-        dup2(fd[0], STDIN_FILENO);
-        close(fd[0]);
-        execvp(command2[0], command2);
-        error("failed to exec command 2");
-    }
-    return 0;
-}
 
 int sh_launch(char **args)// execute command
 {
@@ -165,36 +128,6 @@ char *read_line(void)
     }
 }
 
-char **parse_cmd(char *line)// parse line with TOK_DELIMETERS
-{
-    int bufsize = TOK_BUFSIZE, position = 0;
-    char **tokens = malloc(sizeof(char*) * bufsize);
-    char *token;
-    if(!tokens)
-    {
-        printf("Allocation Fail.\n");
-        exit(EXIT_FAILURE);
-    }
-    token = strtok(line, CMD_DELIMITERS);
-    while(token != NULL)
-    {
-        tokens[position] = token;
-        position++;
-        if(position >= bufsize){
-            bufsize += TOK_BUFSIZE;
-            tokens = realloc(tokens, sizeof(char*) * bufsize);
-            if(!tokens)
-            {
-                printf("Allocation Fail.\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-        token = strtok(NULL, CMD_DELIMITERS);
-    }
-    tokens[position] = NULL;
-    return tokens;
-}
-
 char **parse_line(char *line)// parse line with TOK_DELIMETERS
 {
     int bufsize = TOK_BUFSIZE, position = 0;
@@ -228,38 +161,15 @@ char **parse_line(char *line)// parse line with TOK_DELIMETERS
 void shell_loop(void)
 {
     char *line;
-    char **cmd;
     char **args;
-    char **cmd_list[100];
     int status;
     do 
     {
-        int i = 0;
         printf("%% ");
         line = read_line();
-        cmd = parse_cmd(line);
-        while(cmd[i])
-        {
-            args = parse_line(cmd[i]);
-            // status = sh_execute(args);
-            cmd_list[i] = args;
-            i++;
-        }
-        if(i > 1)
-        {
-            int j = 0;
-            while(cmd_list[j+1])
-            {
-                cisshPipe(cmd_list[j], cmd_list[j+1]);
-                j++;
-            }
-        }
-        else
-        {
-            status = sh_execute(args);
-        }
+        args = parse_line(line);
+        status = sh_execute(args);
         free(line);
-        free(cmd);
         free(args);
     }while (status);
 }
